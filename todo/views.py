5c27e3bf94c5fe7359947bd82d3ca033993bd6cc
rayomeara <rayomeara@gmail.com>
2020-01-08 18:10:37 +0000

    Initial commit for django_todo application

---
+++ todo/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, HttpResponse
+from .models import Item
+# Create your views here.
+
+
+def get_todo_list(request):
+    results = Item.objects.all()
+    return render(request, "todo_list.html", {'items': results})
+
+
+def create_an_item(request):
+    return render(request, "item_form.html")
